db65dbe4a18d7d28486cfffb58886adf188149eb
LCore <1406754158@qq.com>
2015-06-25 21:57:43 +0800

    weather实体(温度)

---
+++ weather.go
@@ -0,0 +1,9 @@
+package weather
+
+type weather struct {
+
+    temp                     float32           //温度
+	max_temp                 float32           //最高温度
+	min_temp                 float32           //最低温度
+
+}
